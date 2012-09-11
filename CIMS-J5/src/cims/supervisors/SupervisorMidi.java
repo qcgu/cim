05f0c9f396d4ad9f6c052a6b118570887ea15405
Andrew Gibson <a.gibson@griffith.edu.au>
2012-09-11 11:30:33 +1000

    Cleaned up after merge - unified approach to handling messages and segments
    Signed-off-by: Andrew Gibson <a.gibson@griffith.edu.au>

--- CIMS-J5/src/cims/supervisors/SupervisorMidi.java
+++ CIMS-J5/src/cims/supervisors/SupervisorMidi.java
@@ -1,6 +1,7 @@
 package cims.supervisors;
 
 import java.util.*;
+import java.util.logging.*;
 
 import cims.CimsMaxIO;
 import cims.capturers.CaptureMidi;
@@ -18,29 +19,32 @@ import cims.deciders.DecideMidi_UserControl;
 
 import static cims.supervisors.SupervisorMidi_Globals.*;
 
-
 public class SupervisorMidi implements Supervisor {
 	
 	private CimsMaxIO io;
 	private CaptureMidi capturer;
 	private AnalyseMidi_Silence analyser_silence;
 	private AnalyseMidi_Controls analyser_controls;
-	public AnalyseMidi_Stats analyser_stats;
+	private AnalyseMidi_Stats analyser_stats;
 	private GenerateMidi_Segment generator_segment;
 	private GenerateMidi_NoteMirror generator_note;
 	private DecideMidi_UserControl decider_userControl;
 	private DecideMidi_01 decider_01;
+	private CaptureOutput outputTracker;
 	private Test tester;
 	
-	private static final int SEGMENT_TESTS = 0;
-	//private static final int MESSAGE_TESTS = 1;
-	//private PlayMidi player;
-	private CaptureOutput outputTracker;
+	private static final int MESSAGE_NOTE = 0;
+	private static final int MESSAGE_CONTROL = 1;
+	private static final int SEGMENT = 2;
+	private static final int TEST_MESSAGE_NOTE = 10;
+	private static final int TEST_MESSAGE_CONTROL = 11;
+	private static final int TEST_SEGMENT = 12;
 	
 	public SupervisorMidi(CimsMaxIO ioObj) {
 		this.io = ioObj;
 		sLastMidiMessage = new MidiMessage();
 		sMidiMessageList = new ArrayList<MidiMessage>();
+		sMidiSegment = new MidiSegment();
 		sMidiStartTime=0;
 		
 		//Capture input and output midi
@@ -57,67 +61,76 @@ public class SupervisorMidi implements Supervisor {
 		generator_segment = new GenerateMidi_Segment(this);
 		decider_01.addGenerator(generator_segment);
 		generator_note = new GenerateMidi_NoteMirror(this);
-		
 		//Test
 		tester = new Test(this);
+		//Set Log Level for SupervisorMidi Global Logger
+		LOGGER.setLevel(Level.INFO);
 	}
 	
 	public void dataIn() {
 		int midiData = this.io.inMidi();
-		//this.txtMsg("DataIN: "+midiData);
+		LOGGER.info("DataIN: "+midiData);
 		capturer.in(midiData);
 	}
 	
 	public void controlIn() {
+		LOGGER.info("ControlIN");
 		decider_userControl.input(this.io.key(), this.io.value());	
 	}
 	
 	public void dataOut(int[] message) {
-		//this.txtMsg("dataOut: "+message[0]+"|"+message[1]+"|"+message[2]);
+		LOGGER.info("dataOut: "+message[0]+"|"+message[1]+"|"+message[2]);
 		this.io.outMidi(message);
 		// output capture to stop stuck notes
 		outputTracker.in(message);
 	}
 	
-	public void txtMsg(String msg) {
-		this.io.textOut(msg);
+	public void addMidiMessage(MidiMessage newMessage) {
+		sLastMidiMessage.copy(newMessage);
+		sMidiMessageList.add(sLastMidiMessage);
+		if (newMessage.messageType<MidiMessage.POLY_AFTERTOUCH){
+			LOGGER.info("addMidiMessage: NOTE");
+			this.doNext(MESSAGE_NOTE);
+		} else {
+			LOGGER.info("addMidiMessage: CONTROL");
+			this.doNext(MESSAGE_CONTROL);
+		}
+	}
+
+	public void addMidiSegment(int segmentStart, int segmentEnd) {
+		sMidiSegment = new MidiSegment(segmentStart-1, segmentEnd);
+		LOGGER.info("SEGMENT ADDED: "+segmentStart+" - "+segmentEnd);
+		this.doNext(SEGMENT);	
 	}
 	
-	public void addMidiMessage(MidiMessage newMessage) {
-		if(sTestMode) {
-			// Run Tests
-		} else {
-			// Let the analyser know that there is new midi to analyse
-			if (newMessage.messageType<MidiMessage.POLY_AFTERTOUCH){
-				// Note messages
-				// Run appropriate decider
-
-				//this.txtMsg("addMidiMessage: " + newMessage.pitch + " " + newMessage.velocity);
-				sMidiSegment.add(newMessage);
-
-				//this.txtMsg("Calling Analyser - Note");
-				decider_01.messageIn(newMessage);
-				
-				if(analyser_silence.newMidi()) analyser_silence.analyse();
-				if(analyser_stats.newMidi()) analyser_stats.analyse();
-				if (decider_01.isMirroring()) generator_note.generate();
-			} else {
-				// Controller messages - call appropriate analyser
-				//this.txtMsg("Calling Analyser - Controller");
-				if(analyser_controls.newMidi()) analyser_controls.analyse();
-			}
+	public void doNext(int nextType) {
+		if (sTestMode) nextType=+10;
+		switch(nextType) {
+		case MESSAGE_NOTE:
+			decider_01.messageIn(sLastMidiMessage);
+			if(analyser_silence.newMidi()) analyser_silence.analyse();
+			if(analyser_stats.newMidi()) analyser_stats.analyse();	
+			if (decider_01.isMirroring()) generator_note.generate();
+			break;
+		case MESSAGE_CONTROL:
+			if(analyser_controls.newMidi()) analyser_controls.analyse();
+			break;
+		case SEGMENT:
+			System.gc(); //force garbage collection
+			decider_01.chooseNextAction();
+			break;
+		case TEST_MESSAGE_NOTE:
+			LOGGER.info("RUN MIDIMESSAGE TESTS");
+			//tester.runTests(Test.MESSAGE_TESTS);
+			break;
+		case TEST_MESSAGE_CONTROL:
+			break;
+		case TEST_SEGMENT:
+			LOGGER.info("RUN SEGMENT TESTS");
+			//tester.runTests(Test.SEGMENT_TESTS);
+			break;
 		}
 	}
-
-	public synchronized void addMidiSegment(int segmentStart, int segmentEnd) {	
-		//this.txtMsg("SEGMENT ADDED: "+segmentStart+" - "+segmentEnd);
-		System.gc();
-		if(sTestMode) {
-			tester.runTests(SEGMENT_TESTS);
-		} else {
-			decider_01.chooseNextAction();
-		}
-	}	
 	
 	
 	
@@ -126,13 +139,14 @@ public class SupervisorMidi implements Supervisor {
 	}
 	
 	public synchronized MidiMessage getLastMidiMessage() {
-		MidiMessage lastMidiMessage = new MidiMessage();
-		lastMidiMessage.copy(sLastMidiMessage);
-		return lastMidiMessage;
+		return sLastMidiMessage;
 	}
 	
 	public void allNotesOff() {
 		outputTracker.allNotesOff();
 	}
-		
+	
+	public void txtMsg(String msg) {
+		this.io.textOut(msg);
+	}
 }
