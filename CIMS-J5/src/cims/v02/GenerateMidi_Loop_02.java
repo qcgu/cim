a2acb49be83d5910f98295cef931818325e9805e
Andrew Gibson <a.gibson@griffith.edu.au>
2012-09-12 13:52:29 +1000

    Created new v02 folder - copy of v01 for further development
    Signed-off-by: Andrew Gibson <a.gibson@griffith.edu.au>

---
+++ CIMS-J5/src/cims/v02/GenerateMidi_Loop_02.java
@@ -0,0 +1,13 @@
+package cims.v02;
+
+import cims.generators.GenerateMidi_Loop;
+import cims.generators.GenerateMidi_Segment;
+
+public class GenerateMidi_Loop_02 extends GenerateMidi_Loop {
+
+	public GenerateMidi_Loop_02(GenerateMidi_Segment segment) {
+		super(segment);
+		// TODO Auto-generated constructor stub
+	}
+
+}
